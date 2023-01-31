202c70ff0125317eafe969545b79a88ac7bc5ed5
unknown <ganitniduniyanikunj10@gmail.com>
2023-01-31 20:57:31 +0530

    Generic Servlet,Servlet Classes and interfaces

---
+++ Session-2/GenericServletExample/src/main/java/com/simplilearn/demo/TestServlet.java
@@ -0,0 +1,59 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class TestServlet implements Servlet {
+	
+	private ServletConfig config=null;
+	 
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+		config=null;
+		System.out.println("Destroy Called");
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		System.out.println(" "+config);
+		return config;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		System.out.println(" "+config.getServletName());
+		return config.getServletName();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("Inti Called");
+		this.config=config;
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Service Called");
+		PrintWriter out= res.getWriter();
+		out.print("This is My Servlet");
+		
+	}
+	
+	
+
+}

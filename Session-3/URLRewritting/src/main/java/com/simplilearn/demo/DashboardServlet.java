0ee0d705e3be4f5182b5b4e8a7658bdb983fcab8
unknown <ganitniduniyanikunj10@gmail.com>
2023-02-01 20:23:21 +0530

    Session tracking-Url Rewritting

---
+++ Session-3/URLRewritting/src/main/java/com/simplilearn/demo/DashboardServlet.java
@@ -0,0 +1,36 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dashboard")
+public class DashboardServlet extends  HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=resp.getWriter();
+		
+		
+		String tokenFromURL=req.getParameter("userid");
+		
+		if(tokenFromURL!=null)
+			out.print("user Exist: "+ tokenFromURL);
+		else
+			out.print("User Not Exist");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	
+
+}
---
+++ Session-3/URLRewritting/src/main/java/com/simplilearn/demo/LoginServlet.java
@@ -0,0 +1,30 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name=req.getParameter("userid");
+		
+		String myURL="dashboard?userid="+name;
+		resp.sendRedirect(myURL);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+
+	
+}

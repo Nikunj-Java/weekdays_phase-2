32fd444194fd9d1c9469ee62c17cfd314d534a27
unknown <ganitniduniyanikunj10@gmail.com>
2023-02-01 21:01:54 +0530

    Session tracking-Url Rewritting,HiddenForm Fields

---
+++ Session-3/HiddenFormField/src/main/java/com/simplilearn/demo/DashboardServlet.java
@@ -0,0 +1,37 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter  out=resp.getWriter();
+		
+		
+		String TokenFromHidden=req.getParameter("userid");
+		
+		if(TokenFromHidden!=null)
+			out.print("User Exist: "+TokenFromHidden);
+		else
+			out.print("Invalid User");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	
+	
+
+}

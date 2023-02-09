614deee43b4f0196981f14afdadb3df1cd1b0f54
unknown <ganitniduniyanikunj10@gmail.com>
2023-02-09 21:16:04 +0530

    Hibernate collection mapping

---
+++ Session-9/HibernateCollectionMapping/src/main/java/com/simplilearn/demo/Color.java
@@ -0,0 +1,40 @@
+package com.simplilearn.demo;
+
+public class Color {
+	
+	private long COLORID;
+	private String name;
+	
+	public Color() {
+		
+	}
+	
+	
+	public Color(String name) {
+		COLORID=0;
+		this.name = name;
+	}
+
+
+
+
+	public long getCOLORID() {
+		return COLORID;
+	}
+
+	public void setCOLORID(long COLORID) {
+		COLORID = COLORID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
